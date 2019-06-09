d0932b5dc49c46700f4a604ac9ae401aa03a7db2
Snaikergerm8 <vbezhuk@mail.ru>
2019-06-09 14:16:09 +0300

    Пока сделал всё, кроме хард. Если хватит сремени, то займусь и им

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,3 +1,4 @@
+__author__ = 'Бежук Валерий Эдуардович'
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
@@ -5,21 +6,14 @@
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
 def my_round(number, ndigits):
-    newNumber = number * 10**ndigits
-    if (newNumber - (newNumber//1)) > 0.5:
-        newNumberTwo = ((newNumber - newNumber//1)/(10**ndigits))
-        number = ((number - newNumberTwo) + (1/10**ndigits))
-        return number
-    
-# 2.9999967*10**5 = 299999.67
-# 2.1999967*10**5 = 219999.66999999998
-# Не могу понять, почему во втором случае получается после запятой 669, а не 67.
-# Мне кажется, что из-за этого третий вариант и не отрабатывает.
-# Завтра доработаю. Обещаю не смотреть разбор домашнего задания.
 
+    numberOne = number * 10**ndigits # ввел дополнительные переменные для того, чтобы было понятнее в режиме отладки.
+    numberTwo = (number*10**ndigits)//1
 
+    if numberOne - numberTwo > 0.5:
+       numberOne = ((numberOne + 1) // 1) / 10**ndigits
 
-
+    return numberOne
 
 print(my_round(2.1234567, 5))
 print(my_round(2.1999967, 5))
@@ -33,9 +27,21 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
 
+    strTicketNumber = str(ticket_number)
+    firstSum = 0
+    secondSum = 0
+
+    if len(strTicketNumber) % 2 != 0:
+        return False
+
+    for i in range(len(strTicketNumber) % 2):   #если я всё правильно понял и сделал,
+        firstSum += int(strTicketNumber[i])     #то теперь можно применить эту функцию не только к шестизначным номерам
+        secondSum += int(strTicketNumber[-i - 1])
+
+    return firstSum == secondSum
 
 print(lucky_ticket(123006))
 print(lucky_ticket(12321))
 print(lucky_ticket(436751))
+print(lucky_ticket(43617511)) #добавил еще одну проверку, чтобы удостовериться
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -1,9 +1,27 @@
+__author__ = 'Бежук Валерий Эдуардович'
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+
+    fib = []
+    sumFib = 0
+    firstSum = n
+    secondSum = 0
+
+    while (sumFib < m):
+        sumFib = firstSum + secondSum
+        firstSum = secondSum
+        secondSum = sumFib
+        if sumFib > m:
+            break
+
+        fib.append(sumFib)
+
+    return fib
+
+print(fibonacci(1,20)) #добавил проверку. Надеюсь, я правильно понял условие.
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -11,8 +29,17 @@ def fibonacci(n, m):
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
 
 
+
 def sort_to_max(origin_list):
-    pass
+    sortList = []
+    sortOriginList = list(origin_list)
+    while len(sortList) != len(origin_list):
+        m = min(sortOriginList)
+        sortList.append(min(sortOriginList))
+        sortOriginList.remove(m)
+    return sortList
+
+
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
@@ -20,8 +47,33 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+my_list = ['да','нет','да','да','не знаю','может быть ','ничего','да борода',
+           'никогда','нет','никак нет','да','да','да джигурда']
+
+def my_filter(fun , l):
+
+    fil_list = []
+    for i in l:
+        if fun(i) == True:
+            fil_list.append(i)
+
+    return fil_list
+
+
+print(my_filter(lambda x: x == 'да', my_list))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+A1, A2, A3, A4 = (5, 5), (2, 4), (6, 3), (9, 4)
+
+def parallelogram(a, b, c, d):
+    if a[0] - d[0] == b[0] - c[0] and a[1] - d[1] == b[1] - c[1]:
+        print("Стороны попарно параллельны, а значит и равны. Данные точки являются вершинами параллелограмма")
+    else:
+        print("Стороны не равны и не параллельны. Данные точки не являются вершинами параллелограмма")
+
+
+parallelogram (A1, A2, A3, A4)
 
