a4fbeaf434d61ee603f9fcfe73a573768bfd35f2
Snaikergerm8 <vbezhuk@mail.ru>
2019-06-11 23:44:41 +0300

    Кроме хард

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -19,6 +19,28 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+#c re
+line_low = re.findall(r'[a-z]+', line)
+print('Символы в нижнем регистре с использованием модуля re: \n', line_low)
+# к первому решению я пришел сам, а второе честно подсмотрел в интернете , но я его разобрал и понял, как он работает
+
+
+#без re
+# print(ord('A'), ord('Z'))
+symbol = list(map(lambda x: chr(x), list(range(65, 91))))  # Преобразуем список из кодов ANSI в список букв A-Z
+line_new = list(line)
+
+for i, element in enumerate(line_new[:]):
+       for element_2 in symbol:
+              if element == element_2:
+                     line_new[i] = ' '
+
+# Соединение списка в строку методом .join и разбиение строки по символу ' '
+stroka = ''.join(line_new).split(' ')
+
+line_str_2 = [i for i in stroka if i != '']
+print('Символы в нижнем регистре без использованием модуля re: \n', line_str_2)
+
 
 
 # Задание-2:
@@ -45,9 +67,68 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+# 1. Решение с помощью re
+line_2_str = re.findall(r'[a-z]{2}([A-Z]+)[A-Z]{2}', line_2)
+print('Список с использованием модуля re: \n', line_2_str)
+
+# 2. Решение без re
+symbol1 = list(map(lambda x: chr(x), list(range(65, 91))))  # Преобразуем список из кодов ANSI в список букв A-Z
+symbol2 = list(map(lambda x: chr(x), list(range(97, 123))))  # Преобразуем список из кодов ANSI в список букв a-z
+line_new = list(line_2)
+
+lst = []
+i = len(line_new) - 1
+# Находим  символ в верхнем регистре после которого стоят еще два символа в верхнем регистре
+while i >= 0:
+       if line_new[i] in symbol2:
+              lst.append(line_new[i])
+       elif line_new[i] in symbol1 and i <= len(line_new) - 3 and line_new[i + 1] in symbol1 and line_new[
+              i + 2] in symbol1:
+              lst.append(line_new[i])
+       else:
+              lst.append(' ')
+       i -= 1
+lst.reverse()  # Переворачиваем список
+
+i = 0
+lst2 = []
+registr = True  # Начальное условие поиска сортировки символов в верхнем регистре
+# Фильтрация списка.
+while i <= len(lst) - 1:
+       if lst[i] in symbol2:
+              registr = True
+       if lst[i] in symbol1 and lst[i - 1] in symbol2 and lst[i - 2] in symbol2:
+              lst2.append(lst[i])
+              registr = False
+       elif lst[i] in symbol1 and registr == False:
+              lst2.append(lst[i])
+       else:
+              lst2.append(' ')
+       i += 1
+stroka = ''.join(lst2).split(' ')  # Преобразование в строку и разбиение по пробелам
+
+line_str_3 = [i for i in stroka if i != '']  # Формирование выхрдного списка из строки
+print('Список без использованием модуля re: \n', line_str_3)
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+MAX_CHISLO = 2500
+
+# spisok = ''.join([random.randint(0,9) for _ in range(10)])
+spisok = [random.randint(0, 9) for _ in range(MAX_CHISLO)]
+spisok = ''.join(list(map(lambda x: str(x), spisok)))
+print(spisok)
+# Запись в файл
+path = 'data\\' + 'script' + '.txt'
+with open(path, 'w', encoding='UTF-8') as file:
+       file.write(str(spisok))
+
+# Чтение файла
+with open(path, 'r', encoding='UTF-8') as file:
+       stroka_1 = list(file.read())
+print(stroka_1)
