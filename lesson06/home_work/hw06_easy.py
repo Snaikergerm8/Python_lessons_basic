1412757f39b1f98a794cb90f0b4c5f1a29baf847
Snaikergerm8 <vbezhuk@mail.ru>
2019-06-21 23:49:03 +0300

    .

--- lesson06/home_work/hw06_easy.py
+++ lesson06/home_work/hw06_easy.py
@@ -17,32 +17,94 @@ class Triangle:
         len_ab = math.sqrt(math.fabs(ab[0] ** 2 + ab[1] ** 2))
         len_ac = math.sqrt(math.fabs(ac[0] ** 2 + ac[1] ** 2))
         len_bc = math.sqrt(math.fabs(bc[0] ** 2 + bc[1] ** 2))
-        return len_ab, len_ac, len_bc
+        list_len_tr = len_ab, len_ac, len_bc
+        return list_len_tr
+
     def perimeter(self):
         import math
-        #вычисляем периметр
-        p = len_ab + len_ac + len_bc
-        return print(f"Периметр треугольника: {p}")
+        # вычисляем периметр
+        p = self.len_side()[0] + self.len_side()[1] + self.len_side()[2]
+        print(f"Периметр треугольника: {p}")
+        return p
+
     def square(self):
         import math
-        pl = self.p / 2
-        square = math.sqrt(pl*((pl - len_ab)*(pl - len_ac)*(pl - len_bc)))
-        return print(f"Площадь треугольника: {square}")
+        pl = self.perimeter() / 2
+        square = math.sqrt(pl * ((pl - self.len_side()[0]) * (pl - self.len_side()[1]) * (pl - self.len_side()[2])))
+        print(f"Площадь треугольника: {square}")
+        return square
 
-
-    '''def height(self):
-        height_a =
-        height_b =
-        height_c =
-    '''
+    def height(self):
+        import math
+        pl = self.perimeter() / 2
+        height_a = 2 * (
+            math.sqrt(pl * (pl - self.len_side()[0]) * (pl - self.len_side()[1]) * (pl - self.len_side()[2]))) / \
+                   self.len_side()[0]
+        height_b = 2 * (
+            math.sqrt(pl * (pl - self.len_side()[0]) * (pl - self.len_side()[1]) * (pl - self.len_side()[2]))) / \
+                   self.len_side()[1]
+        height_c = 2 * (
+            math.sqrt(pl * (pl - self.len_side()[0]) * (pl - self.len_side()[1]) * (pl - self.len_side()[2]))) / \
+                   self.len_side()[2]
+        print(f"Высоты треугольника:\nВысота а = {height_a}\nВысота b = {height_b}\nВысота c = {height_c}")
 
 
 triangle1 = Triangle([1, 2], [3, 5], [9, 8])
 triangle1.square()
 triangle1.perimeter()
+triangle1.height()
 
 # Задача-2: Написать Класс "Равнобочная трапеция", заданной координатами 4-х точек.
 # Предусмотреть в классе методы:
 # проверка, является ли фигура равнобочной трапецией;
 # вычисления: длины сторон, периметр, площадь.
+class Trapezium:
+    def __init__(self, a, b, c, d):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def len_side(self):
+        import math
+        # вычисляем коодинаты сторон трпеции
+        ab = [self.b[0] - self.a[0], self.b[1] - self.a[1]]
+        bc = [self.c[0] - self.b[0], self.c[1] - self.b[1]]
+        cd = [self.d[0] - self.c[0], self.d[1] - self.c[1]]
+        da = [self.a[0] - self.d[0], self.a[1] - self.d[1]]
+        # вычисляем длину сторон трапеции
+        len_ab = math.sqrt(math.fabs(ab[0] ** 2 + ab[1] ** 2))
+        len_bc = math.sqrt(math.fabs(bc[0] ** 2 + bc[1] ** 2))
+        len_cd = math.sqrt(math.fabs(cd[0] ** 2 + cd[1] ** 2))
+        len_da = math.sqrt(math.fabs(da[0] ** 2 + da[1] ** 2))
+        list_len_tr = len_ab, len_bc, len_cd, len_da
+        # print(f"Сторона ab = {len_ab}\nСторона bc = {len_bc}\nСторона cd = {len_cd}\nСторона da = {len_da}")
+        return len_ab, len_bc, len_cd, len_da
+
+    def check(self):
+        if self.len_side()[0] == self.len_side()[2]:
+            print("Трапеция является равнобокой")
+        else:
+            print("Трапеция не является равнобокой")
+
+    def perimetr(self):
+        p = 0
+        for i in self.len_side():
+            p += i
+        print(f"Периметр трапеции = {p}")
+        return p
+
+    def square(self):
+        import math
+        s = ((self.len_side()[1] + self.len_side()[3]) / 2) * math.sqrt(
+            (self.len_side()[0] ** 2) - ((self.len_side()[3] - self.len_side()[1]) ** 2) / 4)
+        print(f"Площаль трапеции = {s}")
+        return s
+
+
+trapezium1 = Trapezium([1, 2], [3, 5], [4, 5], [6, 8])
+trapezium1.len_side()
+trapezium1.check()
+trapezium1.perimetr()
+trapezium1.square()
 
--- lesson06/home_work/hw06_normal.py
+++ lesson06/home_work/hw06_normal.py
@@ -15,3 +15,67 @@
 #  (Ученик --> Класс --> Учителя --> Предметы)
 # 4. Узнать ФИО родителей указанного ученика
 # 5. Получить список всех Учителей, преподающих в указанном классе
+class Class:
+    def __init__(self, class_room, teacher):
+        self.class_room = class_room
+        self.teacher = list(teacher)
+
+
+class Person:
+    def __init__(self, name, patronymic, surname):
+        self.name = name
+        self.patronymic = patronymic
+        self.surname = surname
+
+    def full_name(self):
+        return self.name + ' ' + self.patronymic + ' ' + self.surname
+
+
+class Student(Person):
+    def __init__(self, name, patronymic, surname, class_room, mather, father):
+        Person.__init__(self, name, patronymic, surname)
+        self._class_room = {'class_num': int(class_room.split()[0]), 'class_char': class_room.split()[1]}
+        self.mather = mather
+        self.father = father
+
+    def full_name(self):
+        return self.name + ' ' + self.patronymic + ' ' + self.surname
+
+    def class_room(self):
+        return "{}{}".format(self._class_room['class_num'], self._class_room['class_char'])
+
+    def show_parents(self):
+        print(f"Родители:\nМать:{self.mather}\nОтец:{self.father}")
+
+    def show_class(self):
+        print(f"Класс: {self._class_room}")
+
+
+class Parents(Person):
+    def __init__(self, name, patronymic, surname):
+        Person.__init__(self, name, patronymic, surname)
+
+
+class Teacher(Person):
+    def __init__(self, name, patronymic, surname, dis):
+        Person.__init__(self, name, patronymic, surname)
+        self.dis = dis
+
+    def full_name(self):
+        return self.name + ' ' + self.patronymic + ' ' + self.surname
+
+
+teacher = [Teacher("Петр", "Семенович", "Кукушкин", "Математика"),
+           Teacher("Федор", "Михайлович", "Иванов", "Русский язык")]
+classes = [Class("5A", [teacher[0], teacher[1]]),
+           Class("6Г", [teacher[1]]),
+           Class("10Б", [teacher[0], teacher[1]])]
+parents = [Person("Семен", "Семенович", "Кукин"),
+           Person("Денис", "Витальевич", "Букин"),
+           Person("Дмитрий", "Александрович", "Иванов")]
+
+for f in classes:
+    print(f.class_room)
+
+for f in classes:
+    print()
